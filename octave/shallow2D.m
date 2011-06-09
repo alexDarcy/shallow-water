a4b0d1254e62d34d8c369c134537319f42968974
Alex <you@example.com>
2011-06-09 22:20:21 +0200

    DCU corrigée + ghost cell in Y

--- octave/shallow2D.m
+++ octave/shallow2D.m
@@ -16,12 +16,12 @@ function shallow2D(toPrint=0)
 
   % Create mesh with ghost cells
   x = 0:stepX:Lx+stepX;
-  y = 0:stepY:Ly+stepY;
+  y = -stepY:stepY:Ly+stepY;
   %z = zeros(nX+1,nY+1)
   nX = length(x);
   nY = length(y);
   % For visu
-  limits = [0;Lx;0;Lx;0;1.5];
+  limits = [0;Lx;0;Ly;0;1.5];
 
   for i=1:nX
     for j=1:nY
@@ -29,6 +29,7 @@ function shallow2D(toPrint=0)
       %dist = sqrt(dist);
       %if (dist <= radius )
       if (x(i) < Lx/2)
+      %if (y(j) < Ly/2)
         h(i,j) = h1;
       else
         h(i,j) = h0;
@@ -47,8 +48,8 @@ function shallow2D(toPrint=0)
   end
 
   xCut = x(1:nX-1);
-  yCut = y(1:nY-1);
-  hCut = q1(1:nX-1,1:nY-1);
+  yCut = y(2:nY-1);
+  hCut = q1(1:nX-1,2:nY-1);
   surf(xCut,yCut,hCut);
   view(110,10);
   axis(limits);
@@ -60,7 +61,7 @@ function shallow2D(toPrint=0)
 
 
 
-  for k=1:150
+  for k=1:15
     for i=1:nX-1
       for j=1:nY-1
         hL = q1(i,j);
@@ -80,9 +81,9 @@ function shallow2D(toPrint=0)
         F3(i) = FTmp(3);
 
         GTmp = riemannY(uTilde, vTilde, cTilde, i, j, q1, q2, q3);
-        G1(i) = GTmp(1);
-        G2(i) = GTmp(2);
-        G3(i) = GTmp(3);
+        G1(j) = GTmp(1);
+        G2(j) = GTmp(2);
+        G3(j) = GTmp(3);
 
       end
     end
@@ -91,17 +92,17 @@ function shallow2D(toPrint=0)
     for i=2:nX-1
       for j=2:nY-1
         q1(i,j) = q1(i,j) - dt/stepX*(F1(i)-F1(i-1));
-        q1(i,j) = q1(i,j) - dt/stepY*(G1(i)-G1(i-1));
+        q1(i,j) = q1(i,j) - dt/stepY*(G1(j)-G1(j-1));
 
         q2(i,j) = q2(i,j) - dt/stepX*(F2(i)-F2(i-1));
-        q2(i,j) = q2(i,j) - dt/stepY*(G2(i)-G2(i-1));
+        q2(i,j) = q2(i,j) - dt/stepY*(G2(j)-G2(j-1));
 
         q3(i,j) = q3(i,j) - dt/stepX*(F3(i)-F3(i-1));
-        q3(i,j) = q3(i,j) - dt/stepY*(G3(i)-G3(i-1));
+        q3(i,j) = q3(i,j) - dt/stepY*(G3(j)-G3(j-1));
       end
     end
 
-    hCut = q1(1:nX-1,1:nY-1);
+    hCut = q1(1:nX-1,2:nY-1);
 
     surf(xCut,yCut,hCut);
     axis(limits);
@@ -136,26 +137,23 @@ function F = riemannX(uTilde, vTilde, cTilde, i, j, q1, q2, q3)
   lambda1 = uTilde-cTilde;
   lambda2 = uTilde;
   lambda3 = uTilde+cTilde;
+  w = 0.5*(phi(lambda1)*alpha1*r1 + phi(lambda2)*alpha2*r2 +phi(lambda3)*alpha3*r3);
 
   F(1) = 0.5*(q2(i,j)+q2(i+1,j));
-  F(1) = F(1) - 0.5*(phi(lambda1)*alpha1*r1(1)
-  + phi(lambda2)*alpha2*r2(1) +phi(lambda3)*alpha3*r3(1));
 
   F(2) = 0.5*(q2(i)^2/q1(i)+0.5*g*q1(i)^2 + q2(i+1)^2/q1(i+1)+0.5*g*q1(i+1)^2);
-  F(2) = F(2) - 0.5*(phi(lambda1)*alpha1*r1(2)
-  + phi(lambda2)*alpha2*r2(2) + phi(lambda3)*alpha3*r3(2));
 
   F(3) = 0.5*(q2(i,j)*q3(i,j)/q1(i,j)+q2(i+1,j)*q3(i+1,j)/q1(i+1,j)) ;
-  F(3) = F(3) - 0.5*(phi(lambda1)*alpha1*r1(3)
-  + phi(lambda2)*alpha2*r2(3) + phi(lambda3)*alpha3*r3(3));
+  
+  F = F - w;
 end
 
 % Solves the Riemann problem on y
 function G = riemannY(uTilde, vTilde, cTilde, i, j, q1, q2, q3)
   g = 9.81;
   r1(1) = 1;
-  r1(2) = uTilde-cTilde;
-  r1(3) = vTilde;
+  r1(2) = uTilde;
+  r1(3) = vTilde-cTilde;
   r2(1) = 0;
   r2(2) = -1;
   r2(3) = 0;
@@ -163,37 +161,35 @@ function G = riemannY(uTilde, vTilde, cTilde, i, j, q1, q2, q3)
   r3(2) = uTilde;
   r3(3) = vTilde+cTilde;
 
-  delta(1) = q1(i+1,j)-q1(i,j);
-  delta(2) = q2(i+1,j)-q2(i,j);
-  delta(3) = q3(i+1,j)-q3(i,j);
+  delta(1) = q1(i,j+1)-q1(i,j);
+  delta(2) = q2(i,j+1)-q2(i,j);
+  delta(3) = q3(i,j+1)-q3(i,j);
   alpha1 = ((vTilde+cTilde)*delta(1)-delta(3))/(2*cTilde);
-  alpha2 = uTilde*delta(1)-delta(3);
+  alpha2 = uTilde*delta(1)-delta(2);
   alpha3 = (-(vTilde-cTilde)*delta(1)+delta(3))/(2*cTilde);
   lambda1 = vTilde-cTilde;
   lambda2 = vTilde;
   lambda3 = vTilde+cTilde;
+  w = 0.5*(phi(lambda1)*alpha1*r1 + phi(lambda2)*alpha2*r2 +phi(lambda3)*alpha3*r3);
 
-  G(1) = 0.5*(q3(i,j)+q3(i+1,j));
-  G(1) = G(1) - 0.5*(phi(lambda1)*alpha1*r1(1)
-  + phi(lambda2)*alpha2*r2(1) +phi(lambda3)*alpha3*r3(1));
 
-  G(2) = 0.5*(q2(i,j)*q3(i,j)/q1(i,j)+q2(i+1,j)*q3(i+1,j)/q1(i+1,j)) ;
-  G(2) = G(2) - 0.5*(phi(lambda1)*alpha1*r1(2)
-  + phi(lambda2)*alpha2*r2(2) + phi(lambda3)*alpha3*r3(2));
+  G(1) = 0.5*(q3(i,j)+q3(i,j+1));
+  
+  G(2) = 0.5*(q2(i,j)*q3(i,j)/q1(i,j)+q2(i,j+1)*q3(i,j+1)/q1(i,j+1)) ;
 
-  G(3) = 0.5*(q3(i)^2/q1(i)+0.5*g*q1(i)^2 + q3(i+1)^2/q1(i+1)+0.5*g*q1(i+1)^2);
-  G(3) = G(3) - 0.5*(phi(lambda1)*alpha1*r1(3)
-  + phi(lambda2)*alpha2*r2(3) + phi(lambda3)*alpha3*r3(3));
+  G(3) = 0.5*(q3(i,j)^2/q1(i,j)+0.5*g*q1(i,j)^2 + q3(i,j+1)^2/q1(i,j+1)+0.5*g*q1(i,j+1)^2);
+
+  G = G - w;
 end
 
 % Harten entropy fix
 function z = phi(lambda)
   % empirical value
   epsilon = 2;
-  if (abs(lambda) >= epsilon)
+  %if (abs(lambda) >= epsilon)
     z = abs(lambda);
-  else
-    z = (lambda^2 + epsilon^2)/(2*epsilon);
-  end
+  %else
+  %  z = (lambda^2 + epsilon^2)/(2*epsilon);
+  %end
 end
 
