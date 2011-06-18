436746542d26a13e02448c433ad3a0c4ece6c005
Alex <you@example.com>
2011-06-18 23:42:10 +0200

    oubli visu

---
+++ Visu.h
@@ -0,0 +1,40 @@
+#ifndef __VISU
+#define __VISU
+
+#include <iostream>
+#include "Vector3.h"
+
+using namespace std;
+
+class Visu
+{
+  private:
+    int nbX;
+    int nbZ;
+    int nbPoints;
+    int nbQuads;
+    float Lx;
+    float Lz;
+    float dx;
+    float dz;
+
+
+  public:
+
+    Visu(float* &H, float LX,float nX, float LZ,float nZ);
+
+    ~Visu();
+    void computeColors();
+    void updateNormals();
+    void computeIndices();
+    void updateHeight(float* &H);
+
+    float* vertices;
+    float* normals;
+    int* indices;
+    float* colors;
+};
+
+#endif
+
+
